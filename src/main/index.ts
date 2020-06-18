4202185f4407d2f06520b40dec8295cdf184c602
HiDeo <hideo.sup@gmail.com>
2020-06-18 09:09:34 +0200

    Add main process hot-reload

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow } from 'electron'
 import isDev from 'electron-is-dev'
+import path from 'path'
 
 /**
  * Main browser window.
@@ -10,21 +11,31 @@ let browserWindow: BrowserWindow | null = null
  * Create the main browser window.
  */
 function createBrowserWindow() {
+  // Create the main window.
   browserWindow = new BrowserWindow({
     height: 600,
     width: 800,
   })
 
+  // Load the renderer application.
   browserWindow.loadURL(isDev ? 'http://localhost:3000/index.html' : `file://${__dirname}/../index.html`)
+
+  if (isDev) {
+    // Enable reloading of the main process in dev mode.
+    require('electron-reload')(__dirname, {
+      electron: path.join(__dirname, '..', '..', 'node_modules', '.bin', 'electron'),
+      forceHardReset: true,
+      hardResetMethod: 'exit',
+    })
+
+    // Open the devtools in dev mode.
+    browserWindow.webContents.openDevTools()
+  }
 
   /**
    * Triggered when the window is closed.
    */
   browserWindow.on('closed', () => (browserWindow = null))
-
-  if (isDev) {
-    browserWindow.webContents.openDevTools()
-  }
 }
 
 /**
