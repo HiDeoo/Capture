8b3115e47cce7793de420dfff6a7f34c9b5dd360
HiDeo <hideo.sup@gmail.com>
2020-06-24 16:23:28 +0200

    Restrict file watcher

--- src/main/watcher.ts
+++ src/main/watcher.ts
@@ -4,13 +4,20 @@ import chokidar, { FSWatcher } from 'chokidar'
  * Installs a created file watcher in a specific directory.
  * @param  directoryPath - The directory to watch.
  * @param  callback - The callback to call when a file is created.
+ * @param  extension - The file extension.
  * @return The file watcher.
  */
 export function installCreatedFileWatcher(
   directoryPath: string,
-  callback: (createdFilePath: string) => void
+  callback: (createdFilePath: string) => void,
+  extension = 'png'
 ): FSWatcher {
-  const watcher = chokidar.watch(directoryPath, { ignoreInitial: true, depth: 0 })
+  const watcher = chokidar.watch(`${directoryPath}/*.${extension}`, {
+    depth: 0,
+    // Ignore dotfiles (looks like screencapture can create a temporary hidden file).
+    ignored: /(^|[/\\])\../,
+    ignoreInitial: true,
+  })
 
   watcher.on('add', (filePath: string) => {
     callback(filePath)
