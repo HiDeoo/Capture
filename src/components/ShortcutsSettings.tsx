e8d2e003376a47b8ba557a1c2d5158b1f5c499a3
HiDeo <hideo.sup@gmail.com>
2020-08-19 14:47:48 +0200

    Use a grid to display shortcuts settings

--- src/components/ShortcutsSettings.tsx
+++ src/components/ShortcutsSettings.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import tw, { styled } from 'twin.macro'
 
 import { Group } from './SettingsUi'
 import Shortcut from './Shortcut'
@@ -11,26 +12,34 @@ export const ShortcutsSettingConfiguration = {
   name: 'Shortcuts',
 } as const
 
+const Shortcuts = styled.div`
+  ${tw`grid`}
+
+  grid-template-columns: repeat(auto-fill, minmax(390px, 1fr));
+`
+
 const ShortcutsSettings: React.FC = () => {
   return (
     <>
       <Group title="General">
-        <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
-        <Shortcut name="Do the thing" shortcut="Cmd + 1" />
+        <Shortcuts>
+          <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut readOnly name="Do the thing" shortcut="Cmd + 1" />
+          <Shortcut name="Do the thing" shortcut="Cmd + 1" />
+        </Shortcuts>
       </Group>
     </>
   )
